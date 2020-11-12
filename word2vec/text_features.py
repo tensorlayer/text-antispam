ba486028dde02b47efec7f2b221f81b8b65ab6ea
xiongtianyu <xiongtianyu@mgv.ai>
2020-11-12 10:18:14 +0800

    fix serving

--- word2vec/text_features.py
+++ word2vec/text_features.py
@@ -8,7 +8,9 @@
 
 import numpy as np
 import tensorlayer as tl
-
+import sys
+sys.path.append("../serving/packages")
+from text_regularization import extractWords
 
 wv = tl.files.load_npy_to_any(name='./output/model_word2vec_200.npy')
 for label in ["pass", "spam"]:
@@ -17,6 +19,7 @@ for label in ["pass", "spam"]:
     outp = "output/sample_seq_" + label
     f = open(inp,encoding='utf-8')
     for line in f:
+        line = extractWords(line)
         words = line.strip().split(' ')
         text_sequence = []
         for word in words:
